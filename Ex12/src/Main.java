80559993d754a71babeb885c59423b2392af1c9f
Gabriel Bomfim <gabrielbomfimmotadasilva@gmail.com>
2024-09-19 13:34:58 -0300

    Alteração Código

--- Ex12/src/Main.java
+++ Ex12/src/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static void main(String[] args) {
         int nota;
         double salario, aumento;
-
+        //criação do scanner
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Digite seu salário: ");
@@ -21,25 +21,25 @@ public class Main {
             salario = scanner.nextDouble();
             System.out.print("Digite sua nota: ");
                 nota = scanner.nextInt();
-
+            // Analisando se a nota foi igual ou menor que 1
             if( nota <=1){
                 System.out.println("O seu salário continua o mesmo!");
-
+                // analisando se a nota foi igual ou menor que 2
             } else if(nota <= 2 ){
                 aumento = (salario * 0.05);
                  System.out.println("Aumento de 5%");
                     System.out.printf("R$%.2f", salario + aumento);
-
+                // analisando se a nota foi igual ou menor que 3
             } else if( nota <= 3){
                 aumento = (salario * 0.10);
                   System.out.println("Aumento de 10%");
                         System.out.printf("R$%.2f", salario + aumento);
-            }
+            }// analisando se a nota foi igual ou menor que 4
              else if( nota <= 4){
               aumento = (salario * 0.15);
                 System.out.println("Aumento de 15%");
                     System.out.printf("R$%.2f", salario + aumento);
-        }
+        }// analisando se a nota foi igual ou menor que 5
              else if( nota <= 5){
             aumento = (salario * 0.2);
              System.out.println("Aumento de 20%");
