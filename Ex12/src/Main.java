478fa7dbe3107afbc4c56ab60dcdddb68998ea3f
Gabriel Bomfim <gabrielbomfimmotadasilva@gmail.com>
2024-09-23 08:09:36 -0300

    Alteração Código

--- Ex12/src/Main.java
+++ Ex12/src/Main.java
@@ -11,8 +11,7 @@ public class Main {
     Se o valor da nota for entre 5, o salario tem um aumento de 20%
         */
     public static void main(String[] args) {
-        int nota;
-        double salario, aumento;
+        double salario, aumento,nota;
         //criação do scanner
         Scanner scanner = new Scanner(System.in);
 
@@ -25,30 +24,30 @@ public class Main {
             if( nota <=1){
                 System.out.println("O seu salário continua o mesmo!");
                 // analisando se a nota foi igual ou menor que 2
-            } else if(nota <= 2 ){
+            } else if(nota <= 2.0 ){
                 aumento = (salario * 0.05);
                 // calculamos o salario * a % do aumento
                  System.out.println("Aumento de 5%");
                     System.out.printf("R$%.2f", salario + aumento);
                 // analisando se a nota foi igual ou menor que 3
-            } else if( nota <= 3){
+            } else if( nota <= 3.0){
                 // calculamos o salario * a % do aumento
                 aumento = (salario * 0.10);
                   System.out.println("Aumento de 10%");
                         System.out.printf("R$%.2f", salario + aumento);
             }// analisando se a nota foi igual ou menor que 4
-             else if( nota <= 4){
+             else if( nota <= 4.0){
                 // calculamos o salario * a % do aumento
               aumento = (salario * 0.15);
                 System.out.println("Aumento de 15%");
                     System.out.printf("R$%.2f", salario + aumento);
         }// analisando se a nota foi igual ou menor que 5
-             else if( nota <= 5){
+             else if( nota <= 5.0){
              // calculamos o salario * a % do aumento
             aumento = (salario * 0.2);
              System.out.println("Aumento de 20%");
                 System.out.printf("R$%.2f", salario + aumento);
-        } else if( nota > 5){
+        } if( nota > 5.0){
             System.out.println("Não é possivel ter uma nota maior do que 5");
             }
 
