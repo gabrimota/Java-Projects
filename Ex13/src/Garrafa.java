c0f46a65768c0e93d69c6c57b629462afc5f7c67
Gabriel Bomfim <gabrielbomfimmotadasilva@gmail.com>
2024-10-09 11:58:20 -0300

    java poo study

--- Ex13/src/Garrafa.java
+++ Ex13/src/Garrafa.java
@@ -5,18 +5,30 @@ public class Garrafa {
     float tamanho;
     boolean aberta;
     boolean cheio;
+
     void abrir() {
         this.aberta = true;
     }
 
     void fechar() {
-        this.aberta= true;
+        this.aberta= false;
     }
 
     void encher(){
+        if (this.aberta = false){
+
+            System.out.printf("Abra a garrafa primeiro!");
+        }
+
         this.cheio = true;
+        System.out.printf("A garrafa foi enchida!!");
+
     }
     void esvaziar(){
+        if( cheio = false ){
+            System.out.printf("Não é possivel esvaziar uma garrafa vazia!");
+        }
         this.cheio = false;
+        System.out.printf("A garrafa foi esvaziada!!");
     }
 }
--- Ex13/src/Main.java
+++ Ex13/src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
         Garrafa garrafa = new Garrafa();
         garrafa.cor = "azul";
         garrafa.marca = "aboticario";
         garrafa.tamanho = 20;
         garrafa.cheio = false;
         garrafa.modelo = "plastico";
-        garrafa.abrir();
+
+        garrafa.fechar();
+        garrafa.encher();
 
 
     }
