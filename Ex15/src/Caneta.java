9e8a73ab4f979c5acc287407d2f9ec9dd8e6f500
Gabriel Bomfim <gabrielbomfimmotadasilva@gmail.com>
2024-10-10 14:04:42 -0300

    java poo study  object( public / private / protect )

--- Ex15/src/Caneta.java
+++ Ex15/src/Caneta.java
@@ -1,16 +1,16 @@
 
 // ciração do objeto(classe) chamado caneta , caneta tem suas caracteristicas ( atributos) e seus metodos ( funções).
 public class Caneta {
-    String marca;
-    String cor;
-    float ponta;
-    boolean tampada;
-    String carga;
+    public String marca;
+   public String cor;
+    private float ponta;
+   private boolean tampada;
+    protected int carga;
 
 /* criamos metodos(funções) para utilizarmos no main.java, essas funções podem ter ações como mostrar o status
  da caneta ou destampar ela, rabiscar , escrever. */
 
-    void status(){
+    public  void status(){
       System.out.println("Marca: " + this.marca);
       System.out.println("Cor: " + this.cor);
       System.out.println("Ponta: " + this.ponta);
@@ -18,20 +18,23 @@ public class Caneta {
       System.out.println("Carga: " + this.carga);
 
   }
-   void destampar() {
+  public void destampar() {
         this.tampada = false;
        System.out.println("Caneta destampada");
    }
-   void tampar(){
+    public void tampar(){
         this.tampada = true;
         System.out.println("Caneta tampada");
    }
-  void rabiscar(){
+    public void rabiscar(){
         if( this.tampada ) {
             System.out.println("Destampe primeiro");
         } else {
             System.out.println("rabiscando");
         }
 
+    }
+    public void ponta(){
+        System.out.println("Ponta: " + this.ponta);
     }
 }
--- Ex15/src/Main.java
+++ Ex15/src/Main.java
@@ -1,15 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
         Caneta caneta = new Caneta();
             caneta.cor = "Azul";
             caneta.marca = "Bic";
-            caneta.ponta = 0.5f;
-            caneta.carga = "47%";
-            caneta.tampada = true;
+            caneta.carga = 47;
+
 
             caneta.tampar();
-             caneta.destampar();
+
             caneta.status();
             caneta.rabiscar();
     }
