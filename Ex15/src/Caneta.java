d7e40ad0f9323d5c13f739ca71ed6b1f26827bc1
Gabriel Bomfim <gabrielbomfimmotadasilva@gmail.com>
2024-10-10 11:51:13 -0300

    java poo study  ( public / private / protect )

--- Ex15/src/Caneta.java
+++ Ex15/src/Caneta.java
@@ -1,14 +1,14 @@
 
 // ciração do objeto(classe) chamado caneta , caneta tem suas caracteristicas ( atributos) e seus metodos ( funções).
 public class Caneta {
-    String modelo;
+    String marca;
     String cor;
     float ponta;
     boolean tampada;
     float carga;
 
-  void status(){
-      System.out.println("Modelo: " + modelo);
+  public void status(){
+      System.out.println("Modelo: " + marca);
       System.out.println("Cor: " + cor);
       System.out.println("Ponta: " + ponta);
       System.out.println("Tampada: " + tampada);
--- Ex15/src/Main.java
+++ Ex15/src/Main.java
@@ -1,3 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        Caneta caneta = new Caneta();
+            caneta.cor = "Azul";
+            caneta.marca = "Bic";
 
+
+//            status();
+    }
 }
