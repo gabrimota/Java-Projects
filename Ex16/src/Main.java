39413d776308fe852d11f25b3db942f03adfaac3
Gabriel Bomfim <gabrielbomfimmotadasilva@gmail.com>
2024-10-23 10:39:05 -0300

    java poo study  object( Getters / Setters)

--- Ex16/src/Main.java
+++ Ex16/src/Main.java
@@ -4,7 +4,18 @@ public class Main {
         conta1.setDono("Gabriel Bomfim Mota da Silva");
         conta1.setNumConta(1234);
         conta1.abrirConta("CC");
+        conta1.depositar(100);
         conta1.StatusContaBanco();
         conta1.sacar(100);
+        System.out.println("");
+        ContaBanco conta2 = new ContaBanco();
+        conta2.setDono("Silva");
+        conta2.setNumConta(3333);
+        conta2.abrirConta("CP");
+        conta2.depositar(300);
+        conta2.StatusContaBanco();
+
+
+
     }
 }
