ca8bade5692bdada706532fd260cff68b16aad00
Gabriel Bomfim <gabrielbomfimmotadasilva@gmail.com>
2024-10-24 12:25:50 -0300

    POO Encapsulation Study

--- Ex17/src/Controle.java
+++ Ex17/src/Controle.java
@@ -3,7 +3,7 @@
 //eu errei no nome(deveria ser controlador)
 // todos métodos na interface devem ser públicos
 // além de que eles são todos abstratos ( o metodo abstrato ele é previsto porém não implementado )
-public abstract class Controle {
+public  interface Controle {
 
     public abstract void ligar();
 
--- Ex17/src/ControleRemoto.java
+++ Ex17/src/ControleRemoto.java
@@ -1,4 +1,4 @@
-public class ControleRemoto {
+public class ControleRemoto implements Controle {
 //    Atributos
     private int volume;
     private boolean ligado;
@@ -23,5 +23,55 @@ public class ControleRemoto {
         return tocando;
     }
 // metodos abstratos
+
+    @Override
+    public void ligar() {
+
+    }
+
+    @Override
+    public void desligar() {
+
+    }
+
+    @Override
+    public void abrirMenu() {
+
+    }
+
+    @Override
+    public void fecharMenu() {
+
+    }
+
+    @Override
+    public void maisVolume() {
+
+    }
+
+    @Override
+    public void menosVolume() {
+
+    }
+
+    @Override
+    public void ligarMudo() {
+
+    }
+
+    @Override
+    public void desligarMudo() {
+
+    }
+
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
 
 }
