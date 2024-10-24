79e292b75cc5eb89096206070bd40db8a84cb637
Gabriel Bomfim <gabrielbomfimmotadasilva@gmail.com>
2024-10-24 11:37:37 -0300

    POO Encapsulation Study

--- Ex17/src/ControleRemoto.java
+++ Ex17/src/ControleRemoto.java
@@ -3,5 +3,24 @@ public class ControleRemoto {
     private int volume;
     private boolean ligado;
     private boolean tocando;
+//    Getters e Setters
+    private void SetVolume(int volume) {
+        this.volume = volume;
+    }
+    private int GetVolume() {
+        return volume;
+    }
+    private void SetLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+    private boolean GetLigado() {
+        return ligado;
+    }
+    private void tocando(boolean tocando){
+        this.tocando = tocando;
+    }
+    private boolean GetTocando(){
+        return tocando;
+    }
 
 }
