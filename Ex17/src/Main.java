7fa5342c38f40ea2c840a7023a75ff427375dced
Gabriel Bomfim <gabrielbomfimmotadasilva@gmail.com>
2024-10-27 09:17:47 -0300

    POO Encapsulation Study

--- Ex17/src/Main.java
+++ Ex17/src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
             ControleRemoto controle1 = new ControleRemoto();
-
+            controle1.ligar();
+            controle1.abrirMenu();
 
     }
 }
