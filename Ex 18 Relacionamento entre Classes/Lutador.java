1d9ade88b341f2db98aacba09f8ae9c87f7418da
Gabriel Bomfim <gabrielbomfimmotadasilva@gmail.com>
2024-10-28 10:15:36 -0300

    java poo study (Adicionando códigos)

---
+++ Ex 18 Relacionamento entre Classes/Lutador.java
@@ -0,0 +1,11 @@
+public class Lutador {
+
+    private String nome;
+    private int idade;
+    private float altura;
+    private float peso;
+    private string categoria;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+}
