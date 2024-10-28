0184b9477da5501e869a819b2b9a8a269e4b1e09
Gabriel Bomfim <gabrielbomfimmotadasilva@gmail.com>
2024-10-28 11:23:03 -0300

    java poo study (Adicionando códigos)

--- Ex 18 Relacionamento entre Classes/Lutador.java
+++ Ex 18 Relacionamento entre Classes/Lutador.java
@@ -1,5 +1,66 @@
 public class Lutador {
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+//    Métodos
+    public string getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(string categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
     private String nome;
     private int idade;
     private float altura;
@@ -12,33 +73,7 @@ public class Lutador {
         return idade;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public float getAltura() {
-        return altura;
-    }
-
-    public float getPeso() {
-        return peso;
-    }
-
-    public string getCategoria() {
-        return categoria;
-    }
-
-    public int getVitorias() {
-        return vitorias;
-    }
-
-    public int getDerrotas() {
-        return derrotas;
-    }
-
-    public int getEmpates() {
-        return empates;
-    }
+//    Métodos(Funções)
     public void Apresentar(){
 
     }
