506892504d69b7fe0777c12d2ec44f7d028095a4
Gabriel Bomfim <gabrielbomfimmotadasilva@gmail.com>
2024-10-29 11:55:03 -0300

    java poo study (Add code )

--- Ex 18 Relacionamento entre Classes/src/Main.java
+++ Ex 18 Relacionamento entre Classes/src/Main.java
@@ -5,6 +5,9 @@ public class Main {
         l[0] = new Lutador("Pretty Boy", "França", 31,1.75f,68.9f,11,2,1);
 
         l[0].status();
+        l[1] = new Lutador("Putscript", "Brasil",29,1.68f,57.8f,14,2,3);
+
+
 
     }
 }
