982272c45536a1bbd7c519b60d8555b9c33999cb
Gabriel Bomfim <gabrielbomfimmotadasilva@gmail.com>
2024-10-30 11:21:41 -0300

    java poo study (Add code )

--- Ex 18 Relacionamento entre Classes/src/Luta.java
+++ Ex 18 Relacionamento entre Classes/src/Luta.java
@@ -1,6 +1,39 @@
 public class Luta   {
-    private String desafiado;
-    private String desafiante;
+    private Lutador desafiado;
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desa) {
+        this.desafiante = desa;
+    }
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador de) {
+        this.desafiado = de;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rou) {
+        this.rounds = rou;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean apro) {
+        this.aprovada = apro;
+    }
+
+    private Lutador desafiante;
     private int rounds;
     private boolean aprovada;
 
