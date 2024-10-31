fb56be087b8d498d7443269fa37e445f44d569eb
Gabriel Bomfim <gabrielbomfimmotadasilva@gmail.com>
2024-10-31 10:15:56 -0300

    java poo study (Add code class luta )

--- Ex 18 Relacionamento entre Classes/src/Luta.java
+++ Ex 18 Relacionamento entre Classes/src/Luta.java
@@ -44,7 +44,7 @@ public class Luta   {
                 if(desafiante.getCategoria().equals(desafiado.getCategoria())){
                 } else if (desafiante != desafiado) {
                     setAprovada(true);
-                    System.out.println("Luta entre" + getDesafiado() +" e "+ getDesafiante()+ " Aprovada!");
+                    System.out.println("Luta entre" + desafiante.getNome() +" e "+ desafiado.getNome()+ " Aprovada!");
                 }
     }
     public void Lutar() {
