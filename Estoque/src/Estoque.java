515f48e22fd7beb48e5324abe84b328ab8904a16
Gabriel Bomfim <144559148+gabrimota@users.noreply.github.com>
2025-03-31 18:07:12 -0300

    Adicionando Setters e Getters
    Add Get e Set de cada atributo

--- Estoque/src/Estoque.java
+++ Estoque/src/Estoque.java
@@ -4,7 +4,32 @@ public class Estoque {
     private String nome;
     private int quantidade;
     private Double preco;
-    
     LocalDate data = LocalDate.now();
+
+    private void SetNome(String nome) {
+        this.nome = nome;
+    }
+    private String getNome() {
+        return nome;
+    }
+    private void SetQuantidade(int quantidade){
+        this.quantidade = quantidade;
+    }
+    private int GetQuantidade(){
+        return quantidade;
+    }
+    private void SetPreco(Double preco){
+        this.preco = preco;
+    }
+    private double GetPreco(){
+        return preco;
+    }
+    private void SetData(LocalDate data){
+        this.data = data;
+    }
+
+
+
+
 
 }
