1a1a901cf07768e07329ea9be5e48a3b940945c7
Gabriel Bomfim <144559148+gabrimota@users.noreply.github.com>
2025-07-30 17:19:34 -0300

    Final code

--- SquareArea/src/Main.java
+++ SquareArea/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        double areax, areay;
+        Square x , y;
+        x = new Square();
+        y = new Square();
+
+        System.out.println("Enter the values of square x: ");
+            x.a = sc.nextDouble();
+            x.b = sc.nextDouble();
+            x.c = sc.nextDouble();
+        System.out.println("Enter the values of square y: ");
+            y.a = sc.nextDouble();
+            y.b = sc.nextDouble();
+            y.c = sc.nextDouble();
+         areax = x.CalculateSquareArea();
+         areay = y.CalculateSquareArea();
+
+        System.out.println("Area of the square X is: " + areax);
+        System.out.println("Area of the square Y is: " + areay);
     }
 }
--- SquareArea/src/Square.java
+++ SquareArea/src/Square.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+import static java.lang.Math.sqrt;
+
 public class Square {
     double a, b, c;
 
-    public Square(double a, double b, double c) {
+    /*public Square(double a, double b, double c) {
         this.a = a;
         this.b = b;
         this.c = c;
+    }*/
+
+    public double CalculateSquareArea(){
+        double p =  (a+b+c) /2;
+        return Math.sqrt(p*(p-a)*(p-b)*(p-c));
+
     }
 }
