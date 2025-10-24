5928b7cd8a480cc0e510102adb797ed6e60b8992
Gabriel Bomfim <144559148+gabrimota@users.noreply.github.com>
2025-10-24 13:24:19 -0300

    Codding in main
    Adding code for class functionality

--- SimpleStock/src/Main.java
+++ SimpleStock/src/Main.java
@@ -1,8 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Stock  product1 , product2, product3;
+
+        product1 = new Stock();
 
 
+        System.out.println("Enter Product Data: ");
+        System.out.println("Product Name: ");
+        product1.ProductName = sc.nextLine();
+        System.out.println("Product Price: ");
+        product1.ProductPrice = sc.nextDouble();
+        System.out.println("Product Quantity in stock: ");
+        product1.ProductQuantity = sc.nextInt();
 
+        System.out.println("Name: " + product1.ProductName + ", Price: " + product1.ProductPrice + ", Quantity: " + product1.ProductQuantity);
+
+        sc.close();
     }
 }
--- SimpleStock/src/Stock.java
+++ SimpleStock/src/Stock.java
@@ -1,20 +1,22 @@
 public class Stock {
 
-    private String ProductName;
-    private double ProductPrice;
-    private int ProductQuantity;
+    public String ProductName;
+    public double ProductPrice;
+    public int ProductQuantity;
 
-public Stock(String ProductName, double ProductPrice, int ProductQuantity){
-    this.ProductName= ProductName;
-    this.ProductPrice = ProductPrice;
-    this.ProductQuantity = ProductQuantity;
-}
+//    public Stock(String ProductName, double ProductPrice, int ProductQuantity){
+//        this.ProductName= ProductName;
+//        this.ProductPrice = ProductPrice;
+//        this.ProductQuantity = ProductQuantity;
+//    }
+
+
 public double TotalValueInStock(){
     double valueProducts = ProductPrice * ProductQuantity;
     System.out.println("Total Value in Stock: " + valueProducts);
     return valueProducts;
 }
-public void AddProducts(int Quantity){
+    public void AddProducts(int Quantity){
     this.ProductQuantity += Quantity;
     System.out.println("New product add sucessfully.");
 }
