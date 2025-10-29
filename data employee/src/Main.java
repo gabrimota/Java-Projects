12eda6548de37421458f94ead5cb4db40231e82c
Gabriel Bomfim <144559148+gabrimota@users.noreply.github.com>
2025-10-28 22:43:28 -0300

    New Code in Main and class

--- data employee/src/Main.java
+++ data employee/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        Employee emp1 = new Employee();
+
+        System.out.println("Enter employee name: ");
+        emp1.name = sc.nextLine();
+        System.out.println("Enter the employee gross salary: ");
+        emp1.GrossSalary = sc.nextDouble();
+        System.out.println("enter the tax of employee salary: ");
+        emp1.tax = sc.nextDouble();
+
+        System.out.println("Employee: " + emp1);
     }
 }
