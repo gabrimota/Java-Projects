e7145ac7e14f703e4b4668c858a7d851138f9149
Gabriel Bomfim <144559148+gabrimota@users.noreply.github.com>
2025-10-28 22:35:42 -0300

    new codes in class of project
    New functions and atributes

--- data employee/src/Employee.java
+++ data employee/src/Employee.java
@@ -5,6 +5,15 @@ public class Employee {
     public double GrossSalary;
     public double tax;
 
+    public double NetSalary(){
+        return GrossSalary - tax;
+    }
 
+    public double IncreaseSalary(double percentage){
+        return GrossSalary + (GrossSalary * (percentage/100));
+    }
 
+     public String toString(){
+         return this.name + " $" + NetSalary() + " Age: " + age + " Sex: " + sex;
+     }
 }
