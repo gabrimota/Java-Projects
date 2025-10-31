685965c053121407432f2728920bd3e6998e419d
Gabriel Bomfim <144559148+gabrimota@users.noreply.github.com>
2025-10-30 23:22:25 -0300

    new code in main

--- data employee/src/Main.java
+++ data employee/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+//    creation new object empt1
         Employee emp1 = new Employee();
 
         System.out.println("Enter employee name: ");
